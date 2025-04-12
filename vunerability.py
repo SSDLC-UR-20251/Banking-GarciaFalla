42ca0b91a969665d86be8da692855a8e4cb98531
Angel Gabriel Garcia Falla <angelg.garcia@urosario.edu.co>
2025-04-12 11:07:50 -0500

    Update vunerability.py

--- vunerability.py
+++ vunerability.py
@@ -5,22 +5,21 @@ from django.db import connection
 
 def show_user(request, username):
     with connection.cursor() as cursor:
-        # BAD -- Using string formatting
-        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
-        user = cursor.fetchone()
 
         # GOOD -- Using parameters
         cursor.execute("SELECT * FROM users WHERE username = %s", username)
         user = cursor.fetchone()
 
-        # BAD -- Manually quoting placeholder (%s)
-        cursor.execute("SELECT * FROM users WHERE username = '%s'", username)
-        user = cursor.fetchone()
 
 urlpatterns = [url(r'^users/(?P<username>[^/]+)$', show_user)]
 
 # EJEMPLO 2 
-import hashlib
+from argon2 import PasswordHasher
 
-def get_password_hash(password: str, salt: str):
-    return hashlib.sha256(password + salt).hexdigest() # BAD
+def get_initial_hash(password: str):
+    ph = PasswordHasher()
+    return ph.hash(password) # GOOD
+
+def check_password(password: str, known_hash):
+    ph = PasswordHasher()
+    return ph.verify(known_hash, password) # GOOD
